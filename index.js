6b7c528d769774aca4a13f92f33c5d21edbe9d7d
dekaio <47273651+dekaio@users.noreply.github.com>
2020-08-15 10:56:23 +0530

    Quotes display success

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const router = express.Router();
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const quotes = require('./quotes.json')
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.send("Good");
+})
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server is listening on port 3000');
+})
+function getQuote(req,res){
+    if (req.params.id>0 && quotes[req.params.id-1]){
+        res.send(quotes[req.params.id-1].line);
+    }
+    else{
+        var randN = Math.floor((Math.random() * 2));
+        console.log(randN);
+        res.send(quotes[randN].line);
+    }
+}
+app.get("/api/quote",(req,res)=>{
+    getQuote(req,res);
+})
+app.get("/api/quote/:id",(req,res)=>{
+    getQuote(req,res);
+})
+
+//module.exports = router;
